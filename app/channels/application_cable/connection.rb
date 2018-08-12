ec488c9d038b69afebaede4f62156fc60e72a07e
angkiki <gabrielangzw@gmail.com>
2018-08-12 18:29:23 +0800

    re-added connection verification

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,19 +1,19 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    # identified_by :current_user
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+      logger.add_tags 'ActionCable', current_user.id
+    end
     #
-    # def connect
-    #   self.current_user = find_verified_user
-    #   logger.add_tags 'ActionCable', current_user.id
-    # end
-    #
-    # protected
-    # def find_verified_user
-    #   if (verified_user = env['warden'].user)
-    #     verified_user
-    #   else
-    #     reject_unauthorized_connection
-    #   end
-    # end    
+    protected
+    def find_verified_user
+      if user_signed_in?
+        current_user
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
